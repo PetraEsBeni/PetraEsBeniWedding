c2ce3da2f30525c208ff30b9df5957e6f6f3e64c
Petra Sebak <petra.sebak.ext@siemens-healthineers.com>
2025-05-28 10:26:28 +0200

    delete imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import { useRef } from 'react';
 import './App.css';
 import { CountDown } from './Components/CountDown/CountDown';
-import { Feedback } from './Components/Feedback/Feedback';
-import { Footer } from './Components/Footer/Footer';
 import { ImageHeader } from './Components/ImageHeader/imageHeader';
-import { Invitation } from './Components/Invitation/Invitation';
-import { LoveStory } from './Components/LoveStory/LoveStory';
 import { MenuTitle } from './Components/MenuTitle/menutitle';
-import { TimeTable } from './Components/TimeTable/TimeTable';
 
 const App: React.FC = () => {
   const feedbackRef = useRef<HTMLDivElement | null>(null);
