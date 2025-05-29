3304e360a8d286735a777a333261be0e9b4ebbd7
Petra Sebak <petra.sebak.ext@siemens-healthineers.com>
2025-05-29 14:40:02 +0200

    Fix carousel

--- src/Components/ImageHeader/Carousel.tsx
+++ src/Components/ImageHeader/Carousel.tsx
@@ -1,27 +1,61 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import FirstImg from "./first.jpg";
-import SecondImg from "./second.jpg";
-import ThirdImg from "./third.jpg";
+import { images } from "./Images";
 
-const images = [
-    FirstImg,
-    SecondImg,
-    ThirdImg 
-  ];
+type Viewport = "mobile" | "tablet" | "desktop";
 
+interface IState {
+  viewport: Viewport;
+}
 
-export class Carousel extends React.Component {
+export class Carousel extends React.Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      viewport: this.getViewport()
+    };
+  }
+
+  getViewport() {
+    const width = window.innerWidth;
+    if (width <= 600) return "mobile";
+    if (width <= 1199) return "tablet";
+    return "desktop";
+  }
+
+  handleResize = () => {
+    const viewport = this.getViewport();
+    if (viewport !== this.state.viewport) {
+      this.setState({ viewport });
+    }
+  };
+
+  componentDidMount() {
+    window.addEventListener("resize", this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.handleResize);
+  }
+
     public render(){
+       const { viewport } = this.state; 
+       
         return (
             <div className="carousel-container">
             <div className="carousel-wrapper">
-              {images.concat(images).map((image, index) => (
+              {images.concat(images).map((image, index) => {
+                const style = image[viewport];
+                return (
                 <div
                   className="carousel-slide"
                   key={index}
-                  style={{ backgroundImage: `url(${image})` }}
+                  style={{ 
+                    backgroundImage: `url(${image.src})` ,
+                    backgroundPosition: style.backgroundPosition,
+                    minWidth: style.minWidth
+                  }}
                 />
-              ))}
+              )})}
             </div>
           </div>
         );
---
+++ src/Components/ImageHeader/Images.tsx
@@ -0,0 +1,87 @@
+import FirstImg from "./CarouselPhotos/first.jpg";
+import SecondImg from "./CarouselPhotos/second.jpg";
+import ThirdImg from "./CarouselPhotos/third.jpg";
+import FourthImg from "./CarouselPhotos/fourth.jpg";
+import FifthImg from "./CarouselPhotos/fifth.jpg";
+import SixthImg from "./CarouselPhotos/sixth.jpg";
+import SeventhImg from "./CarouselPhotos/seventh.jpg";
+import EighthImg from "./CarouselPhotos/eighth.jpg";
+import NinthImg from "./CarouselPhotos/ninth.jpg";
+import TenthImg from "./CarouselPhotos/tenth.jpg";
+import EleventhImg from "./CarouselPhotos/eleventh.jpg";
+import TwelfthImg from "./CarouselPhotos/twelfth.jpg";
+
+export const images = [
+  {
+    src: FirstImg,
+    mobile: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 20%", minWidth: "6%" }
+  },
+  {
+    src: SecondImg,
+    mobile: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 20%", minWidth: "6%" }
+  },
+  {
+    src: ThirdImg,
+    mobile: { backgroundPosition: "60% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 20%", minWidth: "6%" }
+  },
+  {
+    src: FourthImg,
+    mobile: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 40%", minWidth: "6%" }
+  },
+  {
+    src: FifthImg,
+    mobile: { backgroundPosition: "50% 15%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 15%", minWidth: "6%" }
+  },
+  {
+    src: SixthImg,
+    mobile: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 20%", minWidth: "6%" }
+  },
+  {
+    src: SeventhImg,
+    mobile: { backgroundPosition: "40% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 30%", minWidth: "6%" }
+  },
+  {
+    src: EighthImg,
+    mobile: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 30%", minWidth: "6%" }
+  },
+  {
+    src: NinthImg,
+    mobile: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 35%", minWidth: "6%" }
+  },
+  {
+    src: TenthImg,
+    mobile: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 12%", minWidth: "6%" }
+  },
+  {
+    src: EleventhImg,
+    mobile: { backgroundPosition: "80% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 15%", minWidth: "6%" }
+  },
+  {
+    src: TwelfthImg,
+    mobile: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    tablet: { backgroundPosition: "50% 20%", minWidth: "6%" },
+    desktop: { backgroundPosition: "50% 60%", minWidth: "6%" }
+  }
+];
