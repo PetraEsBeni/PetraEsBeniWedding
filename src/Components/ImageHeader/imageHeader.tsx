915b281733c84da282fa569237067f70a390282c
Petra Sebak <petra.sebak.ext@siemens-healthineers.com>
2025-06-05 17:24:30 +0200

    fixing

--- src/Components/ImageHeader/imageHeader.tsx
+++ src/Components/ImageHeader/imageHeader.tsx
@@ -6,7 +6,6 @@ export class ImageHeader extends React.Component {
     public render(){
         return (
             <td className="container">
-               {/* <img src={myImage} alt="Hátterkép" className="background-image"/>*/}
                <Carousel/>
                 <div className="text-overlay">
                     <img
