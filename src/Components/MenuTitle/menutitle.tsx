b598480a30d2ff1abb964d5afb8ec8fd28dd698f
Petra Sebak <petra.sebak.ext@siemens-healthineers.com>
2025-08-06 15:30:57 +0200

    logo variations

--- src/Components/MenuTitle/menutitle.tsx
+++ src/Components/MenuTitle/menutitle.tsx
@@ -6,6 +6,11 @@ export class MenuTitle extends React.Component {
         return (
             <div className="header">
                 <span className="title">
+                    {/*<img
+                        src="/logo_white_trans.png"
+                        alt="Logo"
+                        className="header-logo"
+                    />*/}
                     Petra & Beni
                     </span>
             </div>
