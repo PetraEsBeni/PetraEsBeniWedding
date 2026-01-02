1511e6caa477c0a0d46e9374f3fdd22e6e76fdc4
Petra Sebak <petra.sebak.ext@siemens-healhineers.com>
2026-01-02 20:36:55 +0100

    Fix link

--- src/Components/Playlist/Playlist.tsx
+++ src/Components/Playlist/Playlist.tsx
@@ -10,7 +10,7 @@ export class Playlist extends React.Component {
                     Az itt ajánlott dalokat továbbítjuk a DJ-nek,
                     aki ezek stílusa alapján állítja össze az esküvő zenei hangulatát.</div>
                 <a
-                    href="https://open.spotify.com/playlist/2uTZioArmDreH6SdTHO1SP?si=EwqEPa9UTF-5qY0ozWSrUg"
+                    href="https://open.spotify.com/playlist/2uTZioArmDreH6SdTHO1SP?si=eafa0e017dbf4a29&pt=41529fbb45b24b64d592ef2d761b71d6"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="musicLink"
