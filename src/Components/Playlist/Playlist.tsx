e39d9809a4306f81bf7e1e14447cd8cd544f4afc
Petra Sebak <petra.sebak.ext@siemens-healthineers.com>
2026-03-24 13:35:28 +0100

    switch spoty url

--- src/Components/Playlist/Playlist.tsx
+++ src/Components/Playlist/Playlist.tsx
@@ -10,7 +10,7 @@ export class Playlist extends React.Component {
                     Az itt ajánlott dalokat továbbítjuk a DJ-nek,
                     aki ezek stílusa alapján állítja össze az esküvő zenei hangulatát.</div>
                 <a
-                    href="https://open.spotify.com/playlist/2uTZioArmDreH6SdTHO1SP?si=ae661d598fa04f35&pt=61aec85715fbf7a14a7108332b05bb7c"
+                    href="https://open.spotify.com/playlist/2uTZioArmDreH6SdTHO1SP?si=bc229e04a713413d&pt=f70fc1fb252007f3f3214ab16437e6ca"
                     target="_blank"
                     rel="noopener noreferrer"
                     className="musicLink"
